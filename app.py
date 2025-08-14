da3f4d7c3eb2d06c49aa17e0f12045bc2ef7136f
Mateo Ochoa <ochoa_1602@hotmail.com>
2025-08-13 19:26:35 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
 import streamlit as st
-import pandas
-import plotly_express
+import pandas as pd
+import plotly.express as px
 
-df = pd.read_csv("")
+car_data = pd.read_csv("vehicles_us.csv")
+
+st.header("Proyecto 7")
+
+check_hist = st.checkbox("Genera un histograma")
+
+if check_hist:
+  st.write("Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
+  histo = px.histogram(car_data, x="odometer")
+  st.plotly_chart(fig, use_container_width=True)
+
+check_disp = st.checkbox("Genera un gráfico de dispersión")
+
+if check_disp:
+  st.write("Creacion de un gráfico de dispersión para el conjunto de datos de anuncios de ventas de coches")
+  disp = px.scatter(car_data)
