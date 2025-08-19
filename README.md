c852cb83b48aa5a6005b7e8cf35bb447841f4b61
Mateo Ochoa <ochoa_1602@hotmail.com>
2025-08-18 20:00:11 -0600

    Cambios en codigo

--- README.md
+++ README.md
@@ -3,4 +3,6 @@
 " El proyecto nos ayuda a generar visualizaciones de un data frame de ventas de automoviles"
 * Se ejecutaron cambios en el archivo app.py, se agrega el codigo para la creacion de los graficos:
 - Se cambiaron los botones por checkmarks en el codigo
-- Se agrego Creado por: Mateo Ochoa
+- Se agrego Creado por: Mateo Ochoa y Tripleten
+- Se dejo un boton y un checkmark para practicar con ambos.
+- Se investigo como hacer el cambio de color en la pagina para que tenga un poco mas de contraste. 
--- app.py
+++ app.py
@@ -2,23 +2,48 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-car_data = pd.read_csv("vehicles_usp.csv")
+st.markdown(
+    """
+    <style>
+    .stApp {
+        background-color: #485878;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
-st.header("Bienvenido al Proyecto 7")
+car_data = pd.read_csv("vehicles_us.csv")
+
+st.header("Bienvenido al Proyecto 7", divider="gray")
 st.write("Creado por: Mateo Ochoa")
+st.header("TRIPLETEN", divider="gray")
 
-check_hist = st.checkbox("Genera un histograma")
+
+styled_df = car_data.sample(25).style.set_properties(**{
+    'background-color': 'lightblue',
+    'color': 'black',
+    'border-color': 'white'
+})
+data_frame = st.checkbox("Visualiza una muestra de la tabla de datos")
+if data_frame:
+    st.dataframe(styled_df)
+
+check_hist = st.checkbox("Genera un histograma")  # Cambio a un boton
 
 if check_hist:
     st.write(
         "Creación de un histograma para el conjunto de datos de anuncios de venta de coches")
-    histo = px.histogram(car_data, x="odometer")
+    histo = px.histogram(car_data, x="odometer",
+                         color_discrete_sequence=px.colors.qualitative.Pastel)
+    histo.update_layout(plot_bgcolor="#073E6B", paper_bgcolor="#073E6B")
+    # Uso de streamlit en lugar de .show() de plotly
     st.plotly_chart(histo, use_container_width=True)
 
-check_disp = st.checkbox("Genera un gráfico de dispersión")
+check_disp = st.button("Genera un gráfico de dispersión")
 
 if check_disp:
     st.write("Creacion de un gráfico de dispersión para el conjunto de datos de anuncios de ventas de coches")
-    # crear un gráfico de dispersión
     disp = px.scatter(car_data, x="odometer", y="price")
-    st.plotly_chart(disp)
+    disp.update_layout(plot_bgcolor="#073E6B", paper_bgcolor="#073E6B")
+    st.plotly_chart(disp)  # Uso de streamlit en lugar de .show() de plotly
